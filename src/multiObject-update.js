aa4484fa237012f2415b477232e9864dd768123e
Et <>
2022-03-05 17:54:04 +0800

    test switch RPC

--- src/multiObject-update.js
+++ src/multiObject-update.js
@@ -10,12 +10,19 @@
 // @author       maple3142
 // @match        https://*.play.farmersworld.io/*
 // @match        https://*wallet.wax.io/*
-// @require      https://unpkg.com/vue@2.6.10/dist/vue.js
+// @require      https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js
 // @require      https://unpkg.com/xfetch-js@0.3.4/xfetch.min.js
+// @run-at      document-start
+// @grant       unsafeWindow
 // @compatible   firefox >=52
 // @compatible   chrome >=55
 // @license      MIT
 // ==/UserScript==
+const originFetch = fetch;
+unsafeWindow.fetch = (...arg) => {
+    console.log('fetch arg', ...arg);
+}
+
 function sleep(time) {
     return new Promise((resolve) => setTimeout(resolve, time));
 }
@@ -26,19 +33,216 @@ Storage.set = function(name, val) {
 Storage.get = function(name) {
     return localStorage.getItem(name);
 }
+let Podium = {};
+
+Podium.keydown = function(k) {
+    var oEvent = document.createEvent('KeyboardEvent');
+
+    // Chromium Hack
+    Object.defineProperty(oEvent, 'keyCode', {
+        get : function() {
+            return this.keyCodeVal;
+        }
+    });
+    Object.defineProperty(oEvent, 'which', {
+        get : function() {
+            return this.keyCodeVal;
+        }
+    });
+
+    if (oEvent.initKeyboardEvent) {
+        oEvent.initKeyboardEvent("keydown", true, true, document.defaultView, k, k, "", "", false, "");
+    } else {
+        oEvent.initKeyEvent("keydown", true, true, document.defaultView, false, false, false, false, k, 0);
+    }
+
+    oEvent.keyCodeVal = k;
+
+    if (oEvent.keyCode !== k) {
+        alert("keyCode mismatch " + oEvent.keyCode + "(" + oEvent.which + ")");
+    }
+
+    document.body.dispatchEvent(oEvent);
+}
+
+
 ;(async function () {
     'use strict'
+
+    await sleep(7000)
+    // let evtObj = document.createEvent('UIEvents');
+    // evtObj.initUIEvent('keydown', true, true, window, 1);
+
+    // delete evtObj.keyCode;
+    // if (typeof evtObj.keyCode === "undefined") {//为了模拟keycode
+    //     Object.defineProperty(evtObj, "keyCode", { value: 40 });
+    // } else {
+    //     evtObj.key = String.fromCharCode(40);
+    // }
+    //
+    // let evtObj2 = document.createEvent('UIEvents');
+    // evtObj2.initUIEvent('keydown', true, true, window, 1);
+    // delete evtObj2.keyCode;
+    // if (typeof evtObj2.keyCode === "undefined") {//为了模拟keycode
+    //     Object.defineProperty(evtObj2, "keyCode", { value: 13 });
+    // } else {
+    //     evtObj2.key = String.fromCharCode(13);
+    // }
+
+    // let a = document.getElementsByTagName("select")[0]
+    // let eventObj = document.createEvent('MouseEvents');
+    // eventObj.initMouseEvent('click',true,true, document.defaultView,1,0,0,795,365,false,false,true,false,0,null);
+    // a.dispatchEvent(eventObj);
+
+
+
+    // a.addEventListener('change',function(){
+    //     alert('changed');
+    // });
+    // a.size = a.children.length
+    // a.focus()
+    // await sleep(1000)
+    //
+    //
+    //
+    // console.log('doing....')
+    // Podium.keydown(40);
+    // await sleep(1000)
+    // Podium.keydown(40);
+    // await sleep(1000)
+    // Podium.keydown(40);
+    // await sleep(1000)
+    // a.options[3].focus()
+    // await sleep(1000)
+    //
+    // a.options[3].selected = true
+    // await sleep(1000)
+    //
+    // a.options[3].click()
+    // await sleep(1000)
+
+
+
+
+    // let ev = $.Event('keypress');
+    // ev.which = 13; // Carriage-return (Enter)
+    // $('body').trigger(ev);
+
+    // document.dispatchEvent(new KeyboardEvent("keydown", {
+    //     key: "down",
+    //     keyCode: 40, // example values.
+    //     code: "ArrowDown", // put everything you need in this object.
+    //     which: 40,
+    //     shiftKey: false, // you don't need to include values
+    //     ctrlKey: false,  // if you aren't going to use them.
+    //     metaKey: false   // these are here for example's sake.
+    // }));
+    // a.dispatchEvent(new KeyboardEvent("keydown", {
+    //     key: "down",
+    //     keyCode: 40, // example values.
+    //     code: "ArrowDown", // put everything you need in this object.
+    //     which: 40,
+    //     shiftKey: false, // you don't need to include values
+    //     ctrlKey: false,  // if you aren't going to use them.
+    //     metaKey: false   // these are here for example's sake.
+    // }));
+    // a.focus()
+    //
+    // let event = document.createEvent( 'KeyboardEvent' );
+    // event.initKeyboardEvent( 'keydown', true, false, null, 0, false, 0, false, 40, 0 );
+    // document.dispatchEvent( event );
+    // a.focus()
+    //
+    // let doc = a.ownerDocument
+    // let win = doc.defaultView || doc.parentWindow
+    // let evt = document.createEvent("KeyboardEvent");
+    // evt.initKeyboardEvent(
+    //     "keydown",
+    //     true,
+    //     false,
+    //     win,
+    //     "U+0009",
+    //     0,
+    //     false,
+    //     false,
+    //     false,
+    //     false,
+    //     false
+    // );
+    // a.focus()
+    //
+    // document.dispatchEvent(evt);
+    //
+    // a.focus()
+    //
+    // let keyCoke = 40
+    // let keyboardEvent = document.createEvent('KeyboardEvent')
+    // let initMethod = typeof keyboardEvent.initKeyboardEvent !== 'undefined' ? 'initKeyboardEvent' : 'initKeyEvent'
+    // keyboardEvent[initMethod]('keydown', true, true, win, false, false, false, false, keyCoke, 0)
+    // document.dispatchEvent(keyboardEvent)
+
+    //////////////////////////////////
+    // a.focus()
+    // const KeyboardEventInit = {key: 'ArrowDown', code: 'ArrowDown', location: 0, repeat: false,
+    //     isComposing: false, bubbles: true, composed: true, charCode: 40, keyCode: 40}
+    // const evtObj = new KeyboardEvent('keydown', KeyboardEventInit)
+    // evtObj.stop = evtObj.stopPropagation
+    // a.dispatchEvent(evtObj)
+    // a.focus()
+    // document.dispatchEvent(evtObj)
+    // a.focus()
+    // document.body.dispatchEvent(evtObj)
+    //
+    // a.focus()
+    // document.body.dispatchEvent(new KeyboardEvent('keydown', KeyboardEventInit))
+    // a.focus()
+    // document.body.dispatchEvent(new KeyboardEvent('keydown', {bubbles: true, cancelable: true, keyCode: 40}))
+    //
+    //
+    // document.body.dispatchEvent(new KeyboardEvent('keydown', {bubbles: true, cancelable: true, keyCode: 13}))
+/////////////////////////////////
+
+
+    // var keyboardEvent = document.createEvent('KeyboardEvent');
+    // var initMethod = typeof keyboardEvent.initKeyboardEvent !== 'undefined' ? 'initKeyboardEvent' : 'initKeyEvent';
+    //
+    // keyboardEvent[initMethod](
+    //     'keydown', // event type: keydown, keyup, keypress
+    //     true, // bubbles
+    //     true, // cancelable
+    //     Window, // view: should be window
+    //     false, // ctrlKey
+    //     false, // altKey
+    //     false, // shiftKey
+    //     false, // metaKey
+    //     40, // keyCode: unsigned long - the virtual key code, else 0
+    //     0, // charCode: unsigned long - the Unicode character associated with the depressed key, else 0
+    // );
+    // a.dispatchEvent(keyboardEvent);
+    // document.dispatchEvent(keyboardEvent);
+
+    // let event = document.createEvent( 'KeyboardEvent' );
+    // event.initKeyboardEvent( 'keydown', true, false, null, 0, false, 0, false, 65, 0 );
+
+
+    // let event = document.createEvent("KeyboardEvent");
+    // event.initKeyboardEvent("keydown",true,true,document.defaultView,"a",0,"Shift",0);
+    // a.dispatchEvent(new Event('change'));
+    // await sleep(1000)
+    // a.dispatchEvent(evtObj2);
+    // await sleep(1000)
+    // a.dispatchEvent(evtObj1);
+    // await sleep(1000)
+
+
+
 
     let notWorksRpcList = Storage.get("notWorksRpcList") || []
     let currentRpc = Number(Storage.get("currentRpc")) || 1
 
-    window.addEventListener('DOMContentLoaded', (event) => {
-        console.log('DOM fully loaded and parsed');
-    });
-
     setTimeout(async function () {
         await start()
-    }, 1000 * 5)
+    }, 1000 * 5000)
 
     async function start() {
         await sleep(1000)
